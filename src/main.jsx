8701259f7e18fdfe099928881a299b571cb37d35
Shawn Smith <sds.smith24@gmail.com>
2024-08-29 21:52:08 -0400

    theme

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ThemeProvider } from '@mui/material'
 import { AuthProvider } from './context/AuthContext.jsx'
 import App from './App.jsx'
+import { theme } from './lib/theme.js'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <ThemeProvider theme={theme}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </ThemeProvider>
   </StrictMode>,
 )
